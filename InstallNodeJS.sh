d854b6e5a6e8e6faae9354232e5efdda7d4bb807
douduck08 <lu10082000@gmail.com>
2016-04-24 01:35:00 +0800

    add InstallNodeJS.sh

---
+++ InstallNodeJS.sh
@@ -0,0 +1,12 @@
+# install nvm
+git clone https://github.com/creationix/nvm.git ~/.nvm
+# add settings into ~/.bashrc:
+echo '# nvm' >> ~/.bashrc
+echo 'export NVM_DIR=~/.nvm' >> ~/.bashrc
+echo '[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"' >> ~/.bashrc
+source .bash_profile
+
+# list available node.js version:
+# nvm ls-remote
+nvm install 5.11.0
+nvm alias default 5.11.0
