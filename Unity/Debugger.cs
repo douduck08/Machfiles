1dbfa3b7ed1ce8348497e4f0d2e9a13cfe294e8b
douduck08 <lu10082000@gmail.com>
2016-05-09 14:19:12 +0800

    Create Debugger.cs

---
+++ Unity/Debugger.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+
+public static class Debugger {
+
+	public static bool DebugMode = false;
+
+	public static void Log(object message) {
+		if (DebugMode) {
+			Debug.Log(message);
+		}
+	}
+	
+	public static void LogWarning(object message) {
+		if (DebugMode) {
+			Debug.LogWarning(message);
+		}
+	}
+
+	public static void LogError(object message) {
+		if (DebugMode) {
+			Debug.LogError(message);
+		}
+	}
+}
