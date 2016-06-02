24555982c4b36f8bd8775f36596d27ac4778245f
douduck08 <lu10082000@gmail.com>
2016-06-03 03:40:35 +0800

    update Log Display

--- Unity/LogDisplay.cs
+++ Unity/LogDisplay.cs
@@ -2,34 +2,51 @@ using UnityEngine;
 using System.Collections;
 
 public class LogDisplay : MonoBehaviour {
+	public bool isOpen = true;
+	public int FontSize = 24;
+
 	private string myLog = "";
 	private GUIStyle style;
-	private Rect rect;
 
 	void OnEnable () {
-		Application.logMessageReceived += show;
+		Application.logMessageReceived += newLog;
 	}
 
 	void OnDisable () {
 		// Remove callback when object goes out of scope
-		Application.logMessageReceived -= show;
+		Application.logMessageReceived -= newLog;
 	}
 
 	void Awake() {
-		int w = Screen.width, h = Screen.height;
-		rect = new Rect(10, 10, w / 2, h - 20);
 		style = new GUIStyle();
 		style.alignment = TextAnchor.UpperLeft;
-		style.fontSize = h / 16;
+		style.fontSize = FontSize;
 		style.normal.textColor = new Color (0.7f, 0.7f, 0.7f, 1.0f);
 	}
 
 	void OnGUI () {
-		GUI.backgroundColor = new Color(0, 0, 0, 0.8f);
-		GUI.Label(rect, myLog, style);
+		if (isOpen) {
+			if (GUI.Button(new Rect(10, 10, 20, 20), "x")) {
+				isOpen = false;
+			}
+			if (GUI.Button(new Rect(60, 10, 20, 20), "<")) {
+				FontSize -= 2;
+				style.fontSize = FontSize;
+			}
+			if (GUI.Button(new Rect(110, 10, 20, 20), ">")) {
+				FontSize += 2;
+				style.fontSize = FontSize;
+			}
+			int w = Screen.width, h = Screen.height;
+			GUI.Label(new Rect(10, 30, w - 20, h - 40), myLog, style);
+		} else {
+			if (GUI.Button(new Rect(10, 10, 20, 20), "o")) {
+				isOpen = true;
+			}
+		}
 	}
 
-	public void show(string logString, string stackTrace, LogType type)	{
+	public void newLog(string logString, string stackTrace, LogType type)	{
 		myLog = logString + "\n" + myLog;
 		if (myLog.Length > 512) {
 			// if myLog is too long, cut it.
