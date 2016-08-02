1524812730ef4cf61eb1e14f7db54a0bdcde02dc
douduck08 <lu10082000@gmail.com>
2016-08-02 23:36:12 +0800

    fix name

--- InstallRuby.sh
+++ InstallRuby.sh
@@ -1,8 +1,8 @@
 # install rbenv
-git clone git://github.com/sstephenson/rbenv.git .rbenv
+git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
 
 # add settings into ~/.bashrc
-echo '# nvm of Ruby' >> ~/.bashrc
+echo '# rbenv of Ruby' >> ~/.bashrc
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 echo 'export RBENV_ROOT=/usr/local/var/rbenv' >> ~/.bashrc
